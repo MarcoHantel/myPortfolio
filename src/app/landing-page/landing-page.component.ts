5820c7039897344d09650e9e93a6aed2a620b874
MarcoHantel <hantelmarco@gmailcom>
2025-09-29 15:02:11 +0200

    first update

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgStyle, NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [NgStyle, NgClass],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss', 
+})
+export class LandingPageComponent {
+hovered = false;
+
+}
+
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { LanguageService } from '../../services/service/language.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent{
+
+  constructor(public langSvc: LanguageService) {} // changeLanguage() muss in den service rein!!
+
+  activeLang: boolean = false;
+  language: string = "";
+
+  // changeLanguage() muss in den service rein!!
+  // changeLanguage(activeLang: boolean, language: string){
+  // activeLang = true; 
+  // console.log("Language changed" + activeLang + language);
+// }
+changeLanguage(){
+  
+}
+
+}
