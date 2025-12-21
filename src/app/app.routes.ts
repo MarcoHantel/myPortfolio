9736ef5224f9703d3d73bd4a9dfed6659be7391f
MarcoHantel <hantelmarco@gmailcom>
2025-12-21 16:55:02 +0100

    save before chang the app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { WhyMeComponent } from './why-me/why-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    // { path: '', component: LandingPageComponent },
+    // { path: '', component: WhyMeComponent },
+    // { path: 'imprint', component: ImprintComponent },
+];
