87b9cae5b50b6cb7fccc0e5612221928511b89b0
MarcoHantel <hantelmarco@gmailcom>
2026-03-07 14:53:03 +0100

    update my skills img, chage from png to svg

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -17,17 +17,28 @@ export class MySkillsComponent {
   readonly shapeHoverKey = 'my-skills-shape';
 
   skillsImg = [
-    'angular.png',
-    'type_script.png',
-    'java_script.png',
-    'html.png',
-    'css.png',
-    'rest_api.png',
-    'firebase.png',
-    'git.png',
-    'material_design.png',
-    'scrum.png'
+    'angular_skill_Icons.svg',
+    'typescript_skill_Icons.svg',
+    'javascript_skill_Icons.svg',
+    'html_skill_Icons.svg',
+    'css_skill_Icons.svg',
+    'restapi_skill_Icons.svg',
+    'git_skill_Icons.svg',
+    'scrum_skill_Icons.svg',
+    'materiald_skill_Icons.svg'
   ];
+
+  skillsText =[
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'REST API',
+    'Git',
+    'Scrum',
+    'Material Design'
+  ]
 
   constructor(
     public hoverSvc: HoverServiceService,
